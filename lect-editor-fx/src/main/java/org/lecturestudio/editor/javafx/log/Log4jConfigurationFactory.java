0da811f4622f0c8b0e4993081f21179c82b83d2d
Alex Andres <andres.alex@protonmail.com>
2022-01-14 16:00:25 +0100

    apps: append app version to log files

--- lect-editor-fx/src/main/java/org/lecturestudio/editor/javafx/log/Log4jConfigurationFactory.java
+++ lect-editor-fx/src/main/java/org/lecturestudio/editor/javafx/log/Log4jConfigurationFactory.java
@@ -23,6 +23,7 @@ import org.lecturestudio.core.log.Log4jXMLConfigurationFactory;
 
 import org.apache.logging.log4j.core.config.Order;
 import org.apache.logging.log4j.core.config.plugins.Plugin;
+import org.lecturestudio.core.model.VersionInfo;
 
 @Plugin(name = "Log4jConfigurationFactory", category = "ConfigurationFactory")
 @Order(10)
@@ -31,6 +32,7 @@ public class Log4jConfigurationFactory extends Log4jXMLConfigurationFactory {
 	public Log4jConfigurationFactory() {
 		AppDataLocator dataLocator = new AppDataLocator("lectureEditorFX");
 
+		System.setProperty("logAppVersion", VersionInfo.getAppVersion());
 		System.setProperty("logFilePath", dataLocator.getAppDataPath());
 	}
 

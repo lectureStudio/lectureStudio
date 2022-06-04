e584e06718ca6847c94d31ded2f8d52e9c97d9fb
Alex Andres <andres.alex@protonmail.com>
2022-06-04 13:00:40 +0200

    swing: added Resizable and ResizableBorder

---
+++ lect-swing/src/main/java/org/lecturestudio/swing/components/Resizable.java
@@ -0,0 +1,178 @@
+/*
+ * Copyright (C) 2022 TU Darmstadt, Department of Computer Science,
+ * Embedded Systems and Applications Group.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.lecturestudio.swing.components;
+
+import javax.swing.JComponent;
+import javax.swing.event.MouseInputAdapter;
+import javax.swing.event.MouseInputListener;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Cursor;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.event.MouseEvent;
+
+public class Resizable extends JComponent {
+
+	public Resizable(Component comp) {
+		this(comp, new ResizableBorder(8));
+	}
+
+	public Resizable(Component comp, ResizableBorder border) {
+		setLayout(new BorderLayout());
+		setBorder(border);
+
+		add(comp);
+		addMouseListener(resizeListener);
+		addMouseMotionListener(resizeListener);
+	}
+
+	@Override
+	public void setBounds(int x, int y, int width, int height) {
+		if (getParent() != null && !getParent().getBounds().isEmpty()) {
+			Rectangle pBounds = getParent().getBounds();
+
+			if (x < pBounds.x) {
+				x = pBounds.x;
+			}
+			if (y < pBounds.y) {
+				y = pBounds.y;
+			}
+			if (width < 30) {
+				width = 30;
+			}
+			if (height < 30) {
+				height = 30;
+			}
+			if (x + width > pBounds.x + pBounds.width) {
+				width = getWidth();
+				x = pBounds.width - width;
+			}
+			if (y + height > pBounds.y + pBounds.height) {
+				height = getHeight();
+				y = pBounds.height - height;
+			}
+		}
+
+		super.setBounds(x, y, width, height);
+	}
+
+	private void resize() {
+		if (getParent() != null) {
+			getParent().revalidate();
+		}
+	}
+
+	MouseInputListener resizeListener = new MouseInputAdapter() {
+
+		private int cursor;
+		private Point startPos = null;
+
+
+		@Override
+		public void mouseMoved(MouseEvent me) {
+			if (hasFocus()) {
+				ResizableBorder border = (ResizableBorder) getBorder();
+				setCursor(Cursor.getPredefinedCursor(border.getCursor(me)));
+			}
+		}
+
+		@Override
+		public void mouseExited(MouseEvent mouseEvent) {
+			setCursor(Cursor.getDefaultCursor());
+		}
+
+		@Override
+		public void mousePressed(MouseEvent me) {
+			ResizableBorder border = (ResizableBorder) getBorder();
+			cursor = border.getCursor(me);
+			startPos = me.getPoint();
+
+			requestFocus();
+			repaint();
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent mouseEvent) {
+			startPos = null;
+		}
+
+		@Override
+		public void mouseDragged(MouseEvent me) {
+			if (startPos != null) {
+				int x = getX();
+				int y = getY();
+				int w = getWidth();
+				int h = getHeight();
+
+				int dx = me.getX() - startPos.x;
+				int dy = me.getY() - startPos.y;
+
+				switch (cursor) {
+					case Cursor.N_RESIZE_CURSOR -> {
+						setBounds(x, y + dy, w, h - dy);
+						resize();
+					}
+					case Cursor.S_RESIZE_CURSOR -> {
+						setBounds(x, y, w, h + dy);
+						startPos = me.getPoint();
+						resize();
+					}
+					case Cursor.W_RESIZE_CURSOR -> {
+						setBounds(x + dx, y, w - dx, h);
+						resize();
+					}
+					case Cursor.E_RESIZE_CURSOR -> {
+						setBounds(x, y, w + dx, h);
+						startPos = me.getPoint();
+						resize();
+					}
+					case Cursor.NW_RESIZE_CURSOR -> {
+						setBounds(x + dx, y + dy, w - dx, h - dy);
+						resize();
+					}
+					case Cursor.NE_RESIZE_CURSOR -> {
+						setBounds(x, y + dy, w + dx, h - dy);
+						startPos = new Point(me.getX(), startPos.y);
+						resize();
+					}
+					case Cursor.SW_RESIZE_CURSOR -> {
+						setBounds(x + dx, y, w - dx, h + dy);
+						startPos = new Point(startPos.x, me.getY());
+						resize();
+					}
+					case Cursor.SE_RESIZE_CURSOR -> {
+						setBounds(x, y, w + dx, h + dy);
+						startPos = me.getPoint();
+						resize();
+					}
+					case Cursor.MOVE_CURSOR -> {
+						var bounds = getBounds();
+						bounds.translate(dx, dy);
+						setBounds(bounds);
+						resize();
+					}
+				}
+
+				setCursor(Cursor.getPredefinedCursor(cursor));
+			}
+		}
+	};
+}
---
+++ lect-swing/src/main/java/org/lecturestudio/swing/components/ResizableBorder.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright (C) 2022 TU Darmstadt, Department of Computer Science,
+ * Embedded Systems and Applications Group.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.lecturestudio.swing.components;
+
+import javax.swing.SwingConstants;
+import javax.swing.border.Border;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Cursor;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Insets;
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.event.MouseEvent;
+import java.awt.geom.Rectangle2D;
+
+public class ResizableBorder implements Border {
+
+	private static final Color BORDER_COLOR = new Color(148, 163, 184);
+
+	private static final Color THUMB_COLOR = new Color(248, 250, 252);
+
+	private static final Color THUMB_BORDER_COLOR = new Color(71, 85, 105);
+
+	private static final int[] LOCATIONS = {
+			SwingConstants.NORTH,
+			SwingConstants.SOUTH,
+			SwingConstants.WEST,
+			SwingConstants.EAST,
+			SwingConstants.NORTH_WEST,
+			SwingConstants.NORTH_EAST,
+			SwingConstants.SOUTH_WEST,
+			SwingConstants.SOUTH_EAST
+	};
+
+	private static final int[] CURSORS = {
+			Cursor.N_RESIZE_CURSOR, Cursor.S_RESIZE_CURSOR,
+			Cursor.W_RESIZE_CURSOR, Cursor.E_RESIZE_CURSOR,
+			Cursor.NW_RESIZE_CURSOR, Cursor.NE_RESIZE_CURSOR,
+			Cursor.SW_RESIZE_CURSOR, Cursor.SE_RESIZE_CURSOR };
+
+	private final int dist;
+
+
+	public ResizableBorder(int dist) {
+		this.dist = dist;
+	}
+
+	@Override
+	public Insets getBorderInsets(Component component) {
+		return new Insets(dist, dist, dist, dist);
+	}
+
+	@Override
+	public boolean isBorderOpaque() {
+		return false;
+	}
+
+	@Override
+	public void paintBorder(Component component, Graphics g, int x, int y,
+			int w, int h) {
+		g.setColor(BORDER_COLOR);
+		g.drawRect(x + dist / 2, y + dist / 2, w - dist, h - dist);
+
+		if (component.hasFocus()) {
+			Graphics2D g2d = (Graphics2D) g;
+
+			for (int location : LOCATIONS) {
+				Rectangle rect = getRectangle(x, y, w, h, location);
+
+				if (rect != null) {
+					Shape shape = new Rectangle.Double(rect.x + 0.5,
+							rect.y + 0.5, rect.width - 1.5, rect.height - 1.5);
+
+					g2d.setColor(THUMB_COLOR);
+					g2d.fill(shape);
+					g2d.setColor(THUMB_BORDER_COLOR);
+					g2d.draw(shape);
+				}
+			}
+		}
+	}
+
+	private Rectangle getRectangle(int x, int y, int w, int h, int location) {
+		return switch (location) {
+			case SwingConstants.NORTH -> new Rectangle(x + w / 2 - dist / 2, y, dist, dist);
+			case SwingConstants.SOUTH -> new Rectangle(x + w / 2 - dist / 2, y + h - dist, dist, dist);
+			case SwingConstants.WEST -> new Rectangle(x, y + h / 2 - dist / 2, dist, dist);
+			case SwingConstants.EAST -> new Rectangle(x + w - dist, y + h / 2 - dist / 2, dist, dist);
+			case SwingConstants.NORTH_WEST -> new Rectangle(x, y, dist, dist);
+			case SwingConstants.NORTH_EAST -> new Rectangle(x + w - dist, y, dist, dist);
+			case SwingConstants.SOUTH_WEST -> new Rectangle(x, y + h - dist, dist, dist);
+			case SwingConstants.SOUTH_EAST -> new Rectangle(x + w - dist, y + h - dist, dist, dist);
+			default -> new Rectangle();
+		};
+	}
+
+	public int getCursor(MouseEvent me) {
+		Component c = me.getComponent();
+		int w = c.getWidth();
+		int h = c.getHeight();
+
+		for (int i = 0; i < LOCATIONS.length; i++) {
+			Rectangle rect = getRectangle(0, 0, w, h, LOCATIONS[i]);
+
+			if (rect != null && rect.contains(me.getPoint())) {
+				return CURSORS[i];
+			}
+		}
+
+		return Cursor.MOVE_CURSOR;
+	}
+}

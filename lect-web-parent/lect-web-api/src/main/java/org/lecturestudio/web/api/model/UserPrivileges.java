ead76ed288e8610930ec1317b4bd962e5e725435
Alex Andres <andres.alex@protonmail.com>
2022-08-19 15:43:39 +0200

    web-abi: retrieve user information with REST api

---
+++ lect-web-parent/lect-web-api/src/main/java/org/lecturestudio/web/api/model/UserPrivileges.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2022 TU Darmstadt, Department of Computer Science,
+ * Embedded Systems and Applications Group.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.lecturestudio.web.api.model;
+
+import java.util.Set;
+
+public class UserPrivileges {
+
+	private Set<UserPrivilege> privileges;
+
+
+	public Set<UserPrivilege> getPrivileges() {
+		return privileges;
+	}
+}
--- lect-web-parent/lect-web-api/src/main/java/org/lecturestudio/web/api/stream/model/CourseParticipant.java
+++ lect-web-parent/lect-web-api/src/main/java/org/lecturestudio/web/api/stream/model/CourseParticipant.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2022 TU Darmstadt, Department of Computer Science,
+ * Embedded Systems and Applications Group.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.lecturestudio.web.api.stream.model;
 
 import java.util.Objects;
@@ -85,6 +103,6 @@ public class CourseParticipant {
 
 	@Override
 	public String toString() {
-		return "[CourseParticipant] " + id;
+		return "[CourseParticipant] " + id + ", name: " + firstName;
 	}
 }

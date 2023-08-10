e67a89779231f872b3cd1a6b5e14425a8d8e2180
Matthew Mason <maurer1413@gmail.com>
2023-08-10 09:34:33 +0200

    Feature/adds camera recording to lecturePresenter
    - CameraRecordingConfiguration: Adds a new class, which holds the configurations for the camera recording. Including properties like the name, format and codec.
    - CameraRecordingException: Adds a new exception class.
    - CameraRecordingService: Adds a new class, which manages the current state of the camera recording process, including the initialization of a muxer.
    - dict_de_DE: adds translation.
    - dict_de_US: adds translation.
    - PresenterConfiguration: adds CameraRecordingConfiguration.
    - RecordingService: Integrates the CameraRecordingService into the RecordingService.
    - start-recording: adds UI components.
    - start-recording_de_DE: adds translation.
    - start-recording_en_US: adds translation.
    - StartRecordingPresenter: adds camera recording and disposal for recording and preview purposes.
    - StartRecordingView: adds interface methods.
    - SwingMainView: fixes tooltip popup weight.
    - SwingStartRecordingView: Adds a camera selection box and a camera preview panel for the UI together with their corresponding controllers.
    - ToolbarPresenter: Integrates the CameraRecordingService into the "startRecording" function, which is called on pressing the record button of the toolbar.
    - WebRtcStreamService: Integrates WebRTC into recordingService.

--- lect-presenter-api/src/main/java/org/lecturestudio/presenter/api/service/CameraRecordingService.java
+++ lect-presenter-api/src/main/java/org/lecturestudio/presenter/api/service/CameraRecordingService.java
@@ -64,15 +64,15 @@ public class CameraRecordingService extends ExecutableBase {
 
 	private final PresenterContext context;
 
-	private Camera camera;
-
 	private final CameraRecordingConfiguration cameraRecordingConfiguration;
-
-	private FFmpegProcessMuxer muxer;
 
 	private final CameraService camService;
 
 	private final FileLectureRecorder recorder;
+
+	private Camera camera;
+
+	private FFmpegProcessMuxer muxer;
 
 	private WebRtcStreamService streamService;
 
@@ -86,7 +86,7 @@ public class CameraRecordingService extends ExecutableBase {
 
 	private CameraFormat closestFormat;
 
-	private Dimension2D fixedOutputSize = new Dimension2D(1152, 648);
+	private Dimension2D fixedOutputSize;
 
 	private BufferedImage bufferedImage;
 
@@ -108,6 +108,10 @@ public class CameraRecordingService extends ExecutableBase {
 		streamingState = event.getState();
 	}
 
+	/**
+	 * Captures the video frames of the camera, when a recording is started during a stream. And adds them to the muxer.
+	 * @param event
+	 */
 	@Subscribe
 	public void onEvent(final LocalVideoFrameEvent event) {
 		if (!started()) return;
@@ -115,7 +119,7 @@ public class CameraRecordingService extends ExecutableBase {
 		try {
 			BufferedImage image;
 			image = convertVideoFrame(frame);
-			addVideoFrame(image);
+			manageVideoFrameAddition(image);
 		}
 		catch (Exception e) {
 			LOG.error("Conversion of video frame failed.", e);
@@ -128,14 +132,17 @@ public class CameraRecordingService extends ExecutableBase {
 
 	/**
 	 * Starts the image capturing process by adding the converted bufferedImages, which are imported from the camera, to the muxer.
-	 * The muxer only gets initialized once at the start of the process, not on pause.
-	 *
 	 * @throws ExecutableException
 	 */
 	@Override
 	protected void startInternal() throws ExecutableException {
 		streamService.setCaptureLocalCameraVideo(true);
 		if (!isMuxerInit) {
+			if(true){
+				fixedOutputSize = new Dimension2D(1152, 648);
+			}else{
+				fixedOutputSize = new Dimension2D(1152, 864);
+			}
 			this.camera = camService.getCamera(cameraRecordingConfiguration.getCameraName());
 			overwriteCameraRecordingConfigurationWithStreamConfiguration(context.getConfiguration().getStreamConfig());
 			this.closestFormat = getNearestCameraFormat(camera, cameraRecordingConfiguration.getCameraFormat());
@@ -149,7 +156,7 @@ public class CameraRecordingService extends ExecutableBase {
 			camera.setImageSize(new Dimension2D(fixedOutputSize.getWidth(), fixedOutputSize.getHeight()));
 			camera.setImageConsumer(bufferedImage -> {
 				try {
-					addVideoFrame(bufferedImage);
+					manageVideoFrameAddition(bufferedImage);
 				}
 				catch (Exception e) {
 					throw new RuntimeException(e);
@@ -192,42 +199,7 @@ public class CameraRecordingService extends ExecutableBase {
 	}
 
 	/**
-	 * The video file, which has been saved to a pre-defined location with a pre-defined name, gets renamed to match the .presenter file name. If this can't be done or the video file is under 1 second long, the video file gets deleted.
-	 *
-	 * @param file The video file, which has been recorded by the camera
-	 * @throws ExecutableException
-	 */
-	public void finishVideoRecordingProcess(File file) throws ExecutableException {
-		muxer.stop();
-		muxer.destroy();
-		isMuxerInit = false;
-		if (closestFormat.getFrameRate() == 0 || frames * 1.0 / closestFormat.getFrameRate() < 1.0) {
-			try {
-				Files.delete(outputVideoPath);
-			}
-			catch (IOException e) {
-				throw new RuntimeException(e);
-			}
-		}
-		else {
-			try {
-				Files.move(outputVideoPath, getSafeVideoFileDestinationPath(file));
-			}
-			catch (IOException e) {
-				try {
-					Files.delete(outputVideoPath);
-				}
-				catch (IOException ex) {
-					throw new ExecutableException(ex);
-				}
-				throw new ExecutableException(e);
-			}
-		}
-	}
-
-	/**
 	 * Initializes the muxer and sets the output path to a pre-defined location with a pre-define name.
-	 *
 	 * @throws ExecutableException
 	 */
 	private void initMuxer() throws ExecutableException {
@@ -253,74 +225,11 @@ public class CameraRecordingService extends ExecutableBase {
 	}
 
 	/**
-	 * Searches for a free destination for the video to be written to.
+	 * This function guarantees that the adjustments made in the settings page correctly affects both the stream configuration and the camera recording configuration by overwriting the data from camera recording configuration with the data from stream configuration.
 	 *
-	 * @param file The video file, which has been recorded by the camera
-	 * @return A video file target path that is not occupied by another file.
-	 */
-	private Path getSafeVideoFileDestinationPath(File file) {
-		if (!Files.exists(Paths.get(FilenameUtils.getFullPath(file.getPath()) + FilenameUtils.getBaseName(file.getPath()) + "-webcam.mp4"))) {
-			return Paths.get(FilenameUtils.getFullPath(file.getPath()) + FilenameUtils.getBaseName(file.getPath()) + "-webcam.mp4");
-		}
-		int attemptCounter = 1;
-		final int MAX_ATTEMPTS = 100;
-		while (attemptCounter < MAX_ATTEMPTS) {
-			if (!Files.exists(Paths.get(FilenameUtils.getFullPath(file.getPath()) + FilenameUtils.getBaseName(file.getPath()) + "-webcam(" + attemptCounter + ").mp4"))) {
-				return Paths.get(FilenameUtils.getFullPath(file.getPath()) + FilenameUtils.getBaseName(file.getPath()) + "-webcam(" + attemptCounter + ").mp4");
-			}
-			else {
-				attemptCounter++;
-			}
-		}
-		return null;
-	}
-
-	/***
-	 * Takes a bufferedImage, converts it and adds it to the muxer.
-	 * @param bufferedImage The image from the camera
-	 */
-	private void muxVideoImage(BufferedImage bufferedImage) {
-		try {
-			BufferedImage converted = new BufferedImage(bufferedImage.getWidth(), bufferedImage.getHeight(), BufferedImage.TYPE_INT_ARGB);
-			Graphics2D g2d = converted.createGraphics();
-			g2d.drawImage(bufferedImage, 0, 0, null);
-			g2d.dispose();
-			muxer.addVideoFrame(converted);
-			frames++;
-		}
-		catch (IOException e) {
-			LOG.error("Start video image rendering failed", e);
-		}
-	}
-
-	private void addVideoFrame(BufferedImage bufferedImage) throws Exception {
-		// Check audio/video timestamp drift.
-		int audioTimeMs = (int) recorder.getElapsedTime();
-		float currentFps = frames / (audioTimeMs / 1000f);
-
-		int frameStepMs = (int) (1000 / closestFormat.getFrameRate());
-		int videoTimeMs = (int) (frames / closestFormat.getFrameRate() * 1000);
-
-		// Drop video frame if ahead of audio.
-		if (currentFps > closestFormat.getFrameRate()) {
-			return;
-		}
-
-		// Add or duplicate video frames if video is behind audio.
-		while (audioTimeMs - videoTimeMs > frameStepMs) {
-			muxVideoImage(bufferedImage);
-
-			videoTimeMs += frameStepMs;
-		}
-
-		// Compare timestamps again, since video may be behind audio again.
-		if ((int) recorder.getElapsedTime() - videoTimeMs > frameStepMs) {
-			muxVideoImage(bufferedImage);
-		}
-	}
-
-	/**
-	 * Transfers the camera configuration data, configured in the settings page, from the streamConfiguration onto the cameraRecordingConfiguration.
+	 * NOTE As of now changing the dimension and quality of the webcam in the settings page only affects the stream configuration.
+	 * The parts of the stream configuration which instantly adopt the adjustments made in the settings page are thus copied over to the camera recording configuration here.
+	 * This is to avoid adding redundant configuration data. Streamlining these camera configurations needs to be addressed in the future.
 	 */
 	private void overwriteCameraRecordingConfigurationWithStreamConfiguration(StreamConfiguration streamConfig) {
 		CameraFormat fixedFormat = new CameraFormat((int) streamConfig.getCameraCodecConfig().getViewRect().getWidth(), (int) streamConfig.getCameraCodecConfig().getViewRect().getHeight(), streamConfig.getCameraCodecConfig().getFrameRate());
@@ -330,6 +239,14 @@ public class CameraRecordingService extends ExecutableBase {
 		cameraRecordingConfiguration.getCameraCodecConfig().setFrameRate(streamConfig.getCameraCodecConfig().getFrameRate());
 	}
 
+	/***
+	 * Gets the nearest supported format from the camera which resembles the desired format from the input.
+	 *
+	 * NOTE As of now the camera format has to be determined before opening the camera. The reason for this is that the CropAndScale method of VideoFrameBuffer will throw an exception if the format either exactly matches the given camera format or does not match it at all.
+	 * @param camera The camera, whose closest supported format needs to be determined.
+	 * @param format The desired format.
+	 * @return The closest supported format to the desired format from the input.
+	 */
 	private CameraFormat getNearestCameraFormat(Camera camera, CameraFormat format) {
 		CameraFormat[] formats = camera.getSupportedFormats();
 		CameraFormat nearest = null;
@@ -376,6 +293,12 @@ public class CameraRecordingService extends ExecutableBase {
 		return nearest;
 	}
 
+	/***
+	 * Converts a video frame into a buffered image.
+	 * @param videoFrame The video frame from the camera.
+	 * @return The converted video frame as a buffered image.
+	 * @throws Exception
+	 */
 	private BufferedImage convertVideoFrame(VideoFrame videoFrame) throws Exception {
 		int width = (int) fixedOutputSize.getWidth();
 		int height = (int) fixedOutputSize.getHeight();
@@ -405,5 +328,109 @@ public class CameraRecordingService extends ExecutableBase {
 		g2d.dispose();
 
 		return converted;
+	}
+
+	/***
+	 * Takes in a frame, converts it and adds it to the muxer.
+	 * @param bufferedImage The image to be added to the muxer.
+	 */
+	private void addVideoFrameToMuxer(BufferedImage bufferedImage) {
+		try {
+			BufferedImage converted = new BufferedImage(bufferedImage.getWidth(), bufferedImage.getHeight(), BufferedImage.TYPE_INT_ARGB);
+			Graphics2D g2d = converted.createGraphics();
+			g2d.drawImage(bufferedImage, 0, 0, null);
+			g2d.dispose();
+			muxer.addVideoFrame(converted);
+			frames++;
+		}
+		catch (IOException e) {
+			LOG.error("Start video image rendering failed", e);
+		}
+	}
+
+	/***
+	 *  Manages how many frames are added to the muxer, by determining the outputted frame rate of the camera and the desired frame rate which has been configured.
+	 * @param bufferedImage The image to be added to the muxer.
+	 * @throws Exception
+	 */
+	private void manageVideoFrameAddition(BufferedImage bufferedImage) throws Exception {
+		// Check audio/video timestamp drift.
+		int audioTimeMs = (int) recorder.getElapsedTime();
+		float currentFps = frames / (audioTimeMs / 1000f);
+
+		int frameStepMs = (int) (1000 / closestFormat.getFrameRate());
+		int videoTimeMs = (int) (frames / closestFormat.getFrameRate() * 1000);
+
+		// Drop video frame if ahead of audio.
+		if (currentFps > closestFormat.getFrameRate()) {
+			return;
+		}
+
+		// Add or duplicate video frames if video is behind audio.
+		while (audioTimeMs - videoTimeMs > frameStepMs) {
+			addVideoFrameToMuxer(bufferedImage);
+
+			videoTimeMs += frameStepMs;
+		}
+
+		// Compare timestamps again, since video may be behind audio again.
+		if ((int) recorder.getElapsedTime() - videoTimeMs > frameStepMs) {
+			addVideoFrameToMuxer(bufferedImage);
+		}
+	}
+
+	/**
+	 * The video file, which has been saved to a pre-defined location with a pre-defined name, gets renamed to match the .presenter file name. If this can't be done or the video file is under 1 second long, the video file gets deleted.
+	 * @param file The video file, which has been recorded by the camera
+	 * @throws ExecutableException
+	 */
+	public void finishCameraRecordingProcess(File file) throws ExecutableException {
+		muxer.stop();
+		muxer.destroy();
+		isMuxerInit = false;
+		if (closestFormat.getFrameRate() == 0 || frames * 1.0 / closestFormat.getFrameRate() < 1.0) {
+			try {
+				Files.delete(outputVideoPath);
+			}
+			catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		else {
+			try {
+				Files.move(outputVideoPath, getSafeVideoFileDestinationPath(file));
+			}
+			catch (IOException e) {
+				try {
+					Files.delete(outputVideoPath);
+				}
+				catch (IOException ex) {
+					throw new ExecutableException(ex);
+				}
+				throw new ExecutableException(e);
+			}
+		}
+	}
+
+	/**
+	 * Determines a valid name for the camera recording in the root of the given file and converts it into a path. If this path is already occupied, another name is determined. This process repeats a finite amount of times until a valid destination path is found.
+	 * @param file The presenter file, which has been saved to a distinct location.
+	 * @return A video file target path that is not occupied by another file.
+	 */
+	private Path getSafeVideoFileDestinationPath(File file) {
+		if (!Files.exists(Paths.get(FilenameUtils.getFullPath(file.getPath()) + FilenameUtils.getBaseName(file.getPath()) + "-webcam.mp4"))) {
+			return Paths.get(FilenameUtils.getFullPath(file.getPath()) + FilenameUtils.getBaseName(file.getPath()) + "-webcam.mp4");
+		}
+		int attemptCounter = 1;
+		final int MAX_ATTEMPTS = 100;
+		while (attemptCounter < MAX_ATTEMPTS) {
+			if (!Files.exists(Paths.get(FilenameUtils.getFullPath(file.getPath()) + FilenameUtils.getBaseName(file.getPath()) + "-webcam(" + attemptCounter + ").mp4"))) {
+				return Paths.get(FilenameUtils.getFullPath(file.getPath()) + FilenameUtils.getBaseName(file.getPath()) + "-webcam(" + attemptCounter + ").mp4");
+			}
+			else {
+				attemptCounter++;
+			}
+		}
+		return null;
 	}
 }

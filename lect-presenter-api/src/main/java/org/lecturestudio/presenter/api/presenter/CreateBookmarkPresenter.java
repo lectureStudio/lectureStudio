d25c594c6b26ed24c9abefc6cb904fbae777970c
Dustin <Dustinri@gmx.de>
2023-10-22 16:28:38 +0200

    fixed errorHandling and added unittests

--- lect-presenter-api/src/main/java/org/lecturestudio/presenter/api/presenter/CreateBookmarkPresenter.java
+++ lect-presenter-api/src/main/java/org/lecturestudio/presenter/api/presenter/CreateBookmarkPresenter.java
@@ -62,8 +62,7 @@ public class CreateBookmarkPresenter extends Presenter<CreateBookmarkView> {
 			showError("bookmark.assign.warning", "bookmark.key.exists", keyStr);
 		}
 		catch (BookmarkExistsException e){
-			String message = MessageFormat.format(context.getDictionary().get("bookmark.exists"), bookmarkService.getBookmarks().getBookmark(keyStr).getPage());
-			showError("bookmark.assign.warning", message);
+			showError("bookmark.assign.warning", "bookmark.exists");
 		}
 		catch (BookmarkException e) {
 			handleException(e, "Create bookmark failed", "bookmark.assign.warning");

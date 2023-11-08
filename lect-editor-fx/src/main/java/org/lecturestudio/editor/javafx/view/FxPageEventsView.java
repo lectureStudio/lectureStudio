5173e5b1e1d9e7736db4ea0531aa4ab00cc9978f
Alex Andres <andres.alex@protonmail.com>
2023-11-08 19:17:51 +0100

    editor-fx: fixed event table height by filling the empty space

--- lect-editor-fx/src/main/java/org/lecturestudio/editor/javafx/view/FxPageEventsView.java
+++ lect-editor-fx/src/main/java/org/lecturestudio/editor/javafx/view/FxPageEventsView.java
@@ -23,6 +23,8 @@ import static java.util.Objects.nonNull;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 
 import java.util.List;
 
@@ -88,6 +90,8 @@ public class FxPageEventsView extends ContentPane implements PageEventsView {
 
 	@FXML
 	private void initialize() {
+		VBox.setVgrow(eventsTableView, Priority.ALWAYS);
+
 		eventsTableView.setItems(FXCollections.observableArrayList());
 		eventsTableView.getSelectionModel().selectedItemProperty().addListener(observable -> {
 			PageEvent selectedItem = eventsTableView.getSelectionModel().getSelectedItem();

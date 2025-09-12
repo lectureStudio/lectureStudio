a0238ce789868b95829da54c2d91d41a2210a3b1
Alex Andres <andres.alex@protonmail.com>
2025-09-12 19:33:41 +0200

    fix: note slide position handling

--- lect-presenter-api/src/main/java/org/lecturestudio/presenter/api/model/NoteSlidePosition.java
+++ lect-presenter-api/src/main/java/org/lecturestudio/presenter/api/model/NoteSlidePosition.java
@@ -18,10 +18,28 @@
 
 package org.lecturestudio.presenter.api.model;
 
+/**
+ * Defines the possible positions where notes can be displayed in relation to slides.
+ * This enum is used to configure the layout and display preferences for notes
+ * in the presenter interface.
+ *
+ * @author Alex Andres
+ */
 public enum NoteSlidePosition {
 
+    /**
+     * Notes are displayed below the slide preview in the main interface.
+     */
     BELOW_SLIDE_PREVIEW,
+
+    /**
+     * Notes are not displayed.
+     */
     NONE,
+
+    /**
+     * Notes are displayed in an external window or component.
+     */
     EXTERNAL
 
 }

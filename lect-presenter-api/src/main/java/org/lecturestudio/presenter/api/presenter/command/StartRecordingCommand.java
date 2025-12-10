bbc9aa98702819cebcbcd576b9b08b3a97678dfb
Alex Andres <andres.alex@protonmail.com>
2025-12-10 17:17:35 +0100

    fix: refine notify-to-record logic in StartRecordingCommand

--- lect-presenter-api/src/main/java/org/lecturestudio/presenter/api/presenter/command/StartRecordingCommand.java
+++ lect-presenter-api/src/main/java/org/lecturestudio/presenter/api/presenter/command/StartRecordingCommand.java
@@ -20,21 +20,49 @@ package org.lecturestudio.presenter.api.presenter.command;
 
 import org.lecturestudio.core.presenter.command.ShowPresenterCommand;
 import org.lecturestudio.core.view.Action;
+import org.lecturestudio.presenter.api.context.PresenterContext;
 import org.lecturestudio.presenter.api.presenter.StartRecordingPresenter;
 
+/**
+ * Command to display the start recording presenter dialog and configure its start action.
+ * This command is responsible for initializing the recording start workflow.
+ *
+ * @author Alex Andres
+ */
 public class StartRecordingCommand extends ShowPresenterCommand<StartRecordingPresenter> {
 
+	/** The action to be executed when the recording starts. */
 	private final Action startAction;
 
 
+	/**
+	 * Creates a new start recording command with the specified start action.
+	 *
+	 * @param startAction the action to execute when recording starts.
+	 */
 	public StartRecordingCommand(Action startAction) {
 		super(StartRecordingPresenter.class);
 
 		this.startAction = startAction;
 	}
 
+	/**
+	 * Executes the command by setting the start action on the presenter.
+	 *
+	 * @param presenter the start recording presenter to configure.
+	 */
 	@Override
 	public void execute(StartRecordingPresenter presenter) {
+		final PresenterContext pContext = (PresenterContext) presenter.getContext();
+		final boolean notifyToRecord = pContext.getNotifyToRecord();
+
+		// Temporarily disable the notify-to-record feature while in settings.
+		pContext.setNotifyToRecord(false);
+
 		presenter.setOnStart(startAction);
+		presenter.setOnClose(() -> {
+			// Re-enable the notify-to-record feature when the view is closed.
+			pContext.setNotifyToRecord(notifyToRecord);
+		});
 	}
 }
